7057538fc214220f077f3d8827039513dbedcc20
luvagu <luiavag@gmail.com>
2021-06-26 11:05:25 -0500

    added boardBuilder and boardSizes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,37 @@
+const boardBuilder = boardSize => {
+	const state = {
+		boardSize,
+		numRed: 0,
+		numBlue: 0,
+		nextTurn: 'red',
+		winMessage: '',
+		lineCoordinates: {},
+		boxColors: {},
+	}
+
+	for (let i = 0; i < 2; i++) {
+		for (let j = 0; j < state.boardSize + 1; j++) {
+			for (let k = 0; k < state.boardSize; k++) {
+				state.lineCoordinates[`${i},${j},${k}`] = 0
+			}
+		}
+	}
+
+	for (let i = 0; i < state.boardSize; i++) {
+		for (let j = 0; j < state.boardSize; j++) {
+			state.boxColors[`${i},${j}`] = 'white'
+		}
+	}
+
+	return state
+}
+
+const boardSizes = [
+	{ size: 5, text: '5x5' },
+	{ size: 10, text: '10x10' },
+	{ size: 15, text: '15x15' },
+]
+
 const App = () => {
 	return (
 		<div className='conatiner'>
@@ -5,7 +39,17 @@ const App = () => {
 				<h1>Dots &amp; Boxes</h1>
 				<div className='flex-container'>
 					<span className='flex-item'>Board size:</span>
-					{/* buttons */}
+					{boardSizes.map(({ size, text }, idx) => (
+						<button
+							key={idx}
+							className={`flex-item ${
+								currentBoardSize === size ? 'selected' : ''
+							}`}
+							onClick={() => changeBoardSize(size)}
+						>
+							{text}
+						</button>
+					))}
 					<div className='flex-item'>Player&apos;s Score:</div>
 					<div className='flex-item red'>{board.numRed}</div>
 					<div className='flex-item blue'>{board.numBlue}</div>
